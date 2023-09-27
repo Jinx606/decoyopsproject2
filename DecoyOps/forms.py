b8e63a790a3ae82a1ee385e66130e348f69f98ea
Jinx <decoy.jinx@gmail.com>
2023-09-27 09:44:16 +0200

    Added docstring to forms.py

--- DecoyOps/forms.py
+++ DecoyOps/forms.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User  # Import the User model
 
 
 class RegistrationForm(UserCreationForm):
+    """Form for user registration.
+
+    This form is based on the UserCreationForm provided by Django's authentication framework.
+    It includes fields for username, email, and password (with password confirmation).
+
+    Attributes:
+        model (User): The User model to use for registration.
+        fields (list): A list of fields to include in the form.
+    """
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
