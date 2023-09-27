1c3605ba8df2a967994a69d32e462d2e3c510533
Jinx <decoy.jinx@gmail.com>
2023-09-27 09:41:45 +0200

    Added docstring to apps.py

--- DecoyOps/apps.py
+++ DecoyOps/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class DecoyopsConfig(AppConfig):
+    """AppConfig for the DecoyOps Django app.
+
+    This class provides configuration for the DecoyOps app, including the default auto field and the app name.
+
+    Attributes:
+        default_auto_field (str): The default auto field for the app's models.
+        name (str): The name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DecoyOps'
