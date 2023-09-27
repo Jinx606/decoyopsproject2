7146cd004e03c9f154b50766ae9dfdd862682b8d
Jinx <decoy.jinx@gmail.com>
2023-09-27 09:39:18 +0200

    Added docstring to admin.py

--- DecoyOps/admin.py
+++ DecoyOps/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
+    """Admin configuration for the Contact model.
+
+    This class customizes the appearance and behavior of the Contact model in the Django admin panel.
+
+    Attributes:
+        list_display (tuple): A tuple of field names to display in the list view of the admin panel.
+    """
     list_display = ('name', 'surname', 'email', 'message')
 
 admin.site.register(Contact, ContactAdmin)
