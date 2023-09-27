c588aecb4a723b3b3bede61c2896f16988c2571d
Jinx <decoy.jinx@gmail.com>
2023-09-27 09:46:34 +0200

    Added docstring to models.py

--- DecoyOps/models.py
+++ DecoyOps/models.py
@@ -3,6 +3,24 @@ from django.db import models
 # Create your models here.
 #I have the model for the contact form below and another class which will display messages in the admin page as opposed to being called contact
 class Contact(models.Model):
+    """Model representing a contact message.
+
+    This model represents a message submitted via a contact form. It stores the name, surname, email address,
+    and the message content.
+
+    Attributes:
+        name (CharField): The name of the person who submitted the message.
+        surname (CharField): The surname of the person who submitted the message.
+        email (EmailField): The email address of the person who submitted the message.
+        message (TextField): The content of the message.
+
+    Methods:
+        __str__(): Returns the email address of the contact as a string.
+
+    Meta:
+        verbose_name (str): A human-readable name for the model in singular form.
+        verbose_name_plural (str): A human-readable name for the model in plural form.
+    """
     name = models.CharField(max_length=250)
     surname = models.CharField(max_length=250)
     email = models.EmailField()
